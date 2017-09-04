048ae7d3a96e593a67826f7b3a118cc6acd995c6
Rahul <rkredux@gmail.com>
2017-09-04 05:30:00 +0530

    Create Chart components from react-sparklines

---
+++ src/components/chart.js
@@ -0,0 +1,22 @@
+import React from "react"; 
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from "react-sparklines"; 
+import _ from "lodash"; 
+
+function average (data){
+	return _.round(_.sum(data)/data.length); 
+}
+
+const Chart = (props) => {
+	return (
+	
+			<Sparklines height={120} width={180} data={props.data}>
+				<SparklinesLine color={props.color} />
+				<SparklinesReferenceLine type="avg"/>
+				<div>{average(props.data)} {props.units}</div>
+			</Sparklines>
+
+	); 
+}
+
+
+export default Chart; 
